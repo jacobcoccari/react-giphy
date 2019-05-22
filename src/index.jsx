782e1005c3205ee71220652d031799c4c3e2168e
jacob coccari <jacob.coccari@gmail.com>
2019-05-22 10:33:15 -0700

    use classes for stateful, functions for stateless.

--- src/index.jsx
+++ src/index.jsx
@@ -15,14 +15,19 @@ import '../assets/stylesheets/application.scss';
 class Hello extends Component {
   constructor (props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
-      clicked: false
+      clicked: false,
+      counter: 0
     };
   }
 
   handleClick() {
     //we want to change the state!
-    this.state.clicked = !this.state.clicked;
+    this.setState ({
+      clicked: !this.state.clicked,
+      counter: this.state.counter + 1
+    });
     //the ! before the statement just means you change it to the opposite.
   }
 
@@ -30,7 +35,7 @@ class Hello extends Component {
     //build and return HTML components
     return (<div className={this.state.clicked ? 'clicked' : null}
     onClick={this.handleClick}>
-    Hello {this.props.name}
+    Hello {this.props.name} {this.state.counter}
     </div>
     )
   }
