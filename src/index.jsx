9df0e8f9fe0f3e8e18dc6f17d77f3ea5e85d6d29
jacob coccari <jacob.coccari@gmail.com>
2019-05-22 10:12:22 -0700

    learning how to use classes in React.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
+// const Hello = (props) => {
+//   return (
+//     <div>
+//       Hello,
+//       Jacob
+//     </div>
+//   );
+// };
+
+class Hello extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      clicked: false
+    };
+  }
+
+  handleClick() {
+    //we want to change the state!
+    this.state.clicked = !this.state.clicked;
+    //the ! before the statement just means you change it to the opposite.
+  }
+
+  render() {
+    //build and return HTML components
+    return (<div className={this.state.clicked ? 'clicked' : null}
+    onClick={this.handleClick}>
+    Hello {this.props.name}
     </div>
-  );
-};
+    )
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<Hello name="Jacob" />, root);
 }
